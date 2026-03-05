59c05fa4b01abf9c47038c5cfb0fb9840e140b92
vfibyfhbev <artemius.valk@gmail.com>
2026-03-05 13:05:18 +0800

    show test-may-not-show errors

--- testing/princple/tests/test_princples.py
+++ testing/princple/tests/test_princples.py
@@ -1,13 +1,33 @@
-#import sys
-#sys.path.append("../src")
-# TODO make it with pip install -e
-from math_demo import add
+#TODO make it with pip install -e .
+# in project root_dir after setup.py defined
 
+
+# Раннее тестирование позволяет сэкономить время позднее
+# Тесты показывают наличие ошибок, а не отсутсвие
+# Тесты не должны дублировать логику тестируемого кода
+# Тесты не должны использовать ВСЕ наборы входных параметров
+# Тесты должны покрывать "кластеры" входных параметров
+# Тесты должны обнаруживать новые ошибки (pescicide paradox)
+# Тесты покрывают как успешные, так и ошибочные кейсы
+
+from math_demo import add, add_with_bug
 
 
 def test_addition():
-    assert 2 + 2 == 4
-    print("Test ADDITION PASSED")
+    assert add(2,2) == 4
+    assert add(0,0) == 0
+    assert add(7,6) == 13
+    print("Test addition passed")
 
-if name == "main":
+
+def test_addition_with_bug():
+    assert add_with_bug(2,2) == 4
+    assert add_with_bug(0,0) == 0
+    assert add_with_bug(7,6) == 13
+    print("Test bugged addition passed")
+
+
+if __name__ == "__main__":
     test_addition()
+    test_addition_with_bug()
+
